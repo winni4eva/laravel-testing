d92a905a0ceb039b2c7cfc9eb66b4db6adffbb57
winni4eva <adams4lyf@gmail.com>
2021-03-11 07:02:26 +0000

    en(): added sample unit and feature tests

---
+++ tests/Feature/BasicTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Box;
+
+class BasicTest extends TestCase
+{
+    public function testBoxContents()
+    {
+        $box = new Box(['toy']);
+        $this->assertTrue($box->has('toy'));
+        $this->assertFalse($box->has('ball'));
+    }
+
+    public function testTakeOneFromTheBox()
+    {
+        $box = new Box(['torch']);
+        $this->assertEquals('torch', $box->takeOne());
+
+        // Null, now the box is empty
+        $this->assertNull($box->takeOne());
+    }
+
+    public function testStartsWithALetter()
+    {
+        $box = new Box(['toy', 'torch', 'ball', 'cat', 'tissue']);
+
+        $results = $box->startsWith('t');
+
+        $this->assertCount(3, $results);
+        $this->assertContains('toy', $results);
+        $this->assertContains('torch', $results);
+        $this->assertContains('tissue', $results);
+
+        // Empty array if passed even
+        $this->assertEmpty($box->startsWith('s'));
+    }
+}
